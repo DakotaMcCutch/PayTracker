48156cf58d4680c39a09a367fc76ad1225179f3d
DakotaMcCutch <D.mccutch@sympatico.ca>
2014-12-27 23:19:11 -0500

    Last Save of the day

---
+++ PayTracker/Paid.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PayTracker
+{
+    partial class Paid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStart = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.cmdBack = new System.Windows.Forms.Button();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.cmdInsert = new System.Windows.Forms.Button();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(16, 57);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(73, 13);
+            this.lblStart.TabIndex = 6;
+            this.lblStart.Text = "Amount Paid: ";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(13, 30);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(148, 20);
+            this.dtpDate.TabIndex = 5;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(13, 13);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(33, 13);
+            this.lblDate.TabIndex = 4;
+            this.lblDate.Text = "Date:";
+            // 
+            // cmdBack
+            // 
+            this.cmdBack.BackColor = System.Drawing.Color.Tomato;
+            this.cmdBack.Location = new System.Drawing.Point(12, 283);
+            this.cmdBack.Name = "cmdBack";
+            this.cmdBack.Size = new System.Drawing.Size(58, 57);
+            this.cmdBack.TabIndex = 54;
+            this.cmdBack.Text = "Go Back";
+            this.cmdBack.UseVisualStyleBackColor = false;
+            this.cmdBack.Click += new System.EventHandler(this.cmdBack_Click);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.BackColor = System.Drawing.Color.Yellow;
+            this.cmdUpdate.Location = new System.Drawing.Point(396, 283);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(167, 57);
+            this.cmdUpdate.TabIndex = 52;
+            this.cmdUpdate.Text = "Update";
+            this.cmdUpdate.UseVisualStyleBackColor = false;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.BackColor = System.Drawing.Color.Red;
+            this.cmdDelete.Location = new System.Drawing.Point(569, 283);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(167, 57);
+            this.cmdDelete.TabIndex = 53;
+            this.cmdDelete.Text = "Delete";
+            this.cmdDelete.UseVisualStyleBackColor = false;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
+            // 
+            // cmdInsert
+            // 
+            this.cmdInsert.BackColor = System.Drawing.Color.GreenYellow;
+            this.cmdInsert.Location = new System.Drawing.Point(223, 283);
+            this.cmdInsert.Name = "cmdInsert";
+            this.cmdInsert.Size = new System.Drawing.Size(167, 57);
+            this.cmdInsert.TabIndex = 51;
+            this.cmdInsert.Text = "Insert";
+            this.cmdInsert.UseVisualStyleBackColor = false;
+            this.cmdInsert.Click += new System.EventHandler(this.cmdInsert_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(13, 74);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(148, 20);
+            this.txtAmount.TabIndex = 55;
+            // 
+            // Paid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 352);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.cmdBack);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.cmdInsert);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.lblDate);
+            this.Name = "Paid";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paid";
+            this.Load += new System.EventHandler(this.Paid_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Button cmdBack;
+        private System.Windows.Forms.Button cmdUpdate;
+        private System.Windows.Forms.Button cmdDelete;
+        private System.Windows.Forms.Button cmdInsert;
+        private System.Windows.Forms.TextBox txtAmount;
+    }
+}
---
+++ PayTracker/Paid.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayTracker
+{
+    public partial class Paid : Form
+    {
+        public Paid()
+        {
+            InitializeComponent();
+        }
+
+        private void startUp()
+        {
+            dtpDate.MinDate = DateTime.Today.AddYears(-99);
+            dtpDate.MaxDate = DateTime.Today;
+            dtpDate.Text = DateTime.Today.ToString();
+            dtpDate.CustomFormat = "MM/dd/yyyy";
+            dtpDate.Format = DateTimePickerFormat.Custom;
+
+
+        }
+
+        private void Paid_Load(object sender, EventArgs e)
+        {
+            startUp();
+            dtpDate.ValueChanged += dtpDate_ValueChanged;
+            FormClosing += Paid_FormClosing;
+        }
+
+        void dtpDate_ValueChanged(object sender, EventArgs e)
+        {
+            SendKeys.Send("{Right}");
+        }
+
+        private void cmdBack_Click(object sender, EventArgs e)
+        {
+            Start s = new Start();
+            s.Show();
+            this.Hide();
+        }
+
+        private void cmdInsert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Paid_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.ApplicationExitCall)
+            {
+                return;
+            }
+            if (System.Windows.Forms.MessageBox.Show(" Do you want to quit?          ", "Quit...", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.Exit();
+                Dispose(true);
+
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
